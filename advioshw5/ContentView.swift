0b3fa7895cb0e91014bda034b1949371e72939d9
diasjakupov <dias.jakupov@zimran.io>
2025-04-01 22:44:08 +0500

    Initial Commit

---
+++ advioshw5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  advioshw5
+//
+//  Created by Dias Jakupov on 01.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ advioshw5/advioshw5App.swift
@@ -0,0 +1,17 @@
+//
+//  advioshw5App.swift
+//  advioshw5
+//
+//  Created by Dias Jakupov on 01.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct advioshw5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
